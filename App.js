bc04c4e8783ad0edf953a1b93b2e11ec2e8305a1
Serbato <lserbato@gmail.com>
2024-03-18 22:50:39 -0300

    Modal pronto

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Modal, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {setModalVisible(!modalVisible)}}
+      >
+        <View style={styles.modalView}>
+          <Text>Exemplo de Modal</Text>
+          <TouchableOpacity onPress={() => setModalVisible(false)}>
+            <Text>Fechar</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+
+      <TouchableOpacity onPress={() => setModalVisible(true)}>
+        <Text style={styles.button}>Clique para abrir o modal</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +37,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    borderRadius: 10,
+    backgroundColor: '#333666',
+    padding: 10,
+    elevation: 2,
+  },
+  modalView: {
+    backgroundColor: '#333666',
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+    elevation: 5,
+    width: '100vw',
+    height: '100vh',
+
   },
 });
